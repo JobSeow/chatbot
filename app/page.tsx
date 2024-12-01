d2cb761dfaff78fce8095a7eb6697d51e0d68a94
jobseow <seowjianliang@gmail.com>
2024-12-01 21:31:20 +0800

    setup

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="w-screen h-screen bg-black justify-center items-center">
+            <div>Home</div>
+        </div>
+    )
+}
